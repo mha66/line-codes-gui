3575b9892d180980791492215c64a425d0448311
mha66 <mharegaily@gmail.com>
2024-06-20 15:28:13 +0300

    implemented base GUI and line codes (MLT3, B8ZS, HDB3)

---
+++ src/linecodesgui/DigitalWave.java
@@ -0,0 +1,192 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package linecodesgui;
+
+import java.util.ArrayList;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
+
+
+
+/**
+ *
+ * @author mhame
+ */
+public class DigitalWave{
+
+    static final int Y_HI = 50;
+    static final int Y_0 = 100;
+    static final int Y_LO = 150;
+    static final int DX = 50;
+   
+    ArrayList<Line> lines = new ArrayList<>();
+    
+ 
+
+    public DigitalWave(String signal) {
+       signal += 'n'; //end char
+       char levels[] = signal.toCharArray();
+       
+       int x = 10;
+       for(int i = 0; i < signal.length() - 1; i++)
+       {
+           if(levels[i] == '0')
+           {
+               lines.add(new Line(x, Y_0, x+DX, Y_0 ));
+               x+=DX;
+               if(levels[i+1] == '+')
+                   lines.add(new Line(x, Y_0, x, Y_HI ));
+               else if(levels[i+1] == '-')
+                   lines.add(new Line(x, Y_0, x, Y_LO ));
+           }
+           else if(levels[i] == '+')
+           {
+               lines.add(new Line(x, Y_HI, x+DX, Y_HI ));
+               x+=DX;
+               if(levels[i+1] == '0')
+                   lines.add(new Line(x, Y_HI, x, Y_0 ));
+               else if(levels[i+1] == '-')
+                   lines.add(new Line(x, Y_HI, x, Y_LO ));
+           }
+           else if(levels[i] == '-')
+           {
+               lines.add(new Line(x, Y_LO, x+DX, Y_LO ));
+               x+=DX;
+               if(levels[i+1] == '0')
+                   lines.add(new Line(x, Y_LO, x, Y_0 ));
+               else if(levels[i+1] == '+')
+                   lines.add(new Line(x, Y_LO, x, Y_HI ));
+           }
+       } 
+    }
+    
+    public static String b8zs(String data){
+        String signal = "";
+        int lastPolarity = -1;
+        
+        data = data.replaceAll("00000000", "000v10v1");
+        
+        for(int i= 0; i<data.length(); i++)
+        {
+            switch (data.charAt(i)) {
+                case '1':
+                    if(lastPolarity > 0)
+                        signal += '-';
+                    else
+                        signal += '+';
+                    lastPolarity *= -1;
+                    break;
+                
+                case 'v':
+                    if(lastPolarity < 0)
+                        signal += '-';
+                    else
+                        signal += '+';
+                    break;
+                case '0':
+                    signal += '0';
+                    break;
+                default:
+                    break;
+                }
+        }
+        return signal;
+      }
+      
+     public static String hdb3(String data){
+        String signal = "";
+        int lastPolarity = -1;
+        int numOnes = 0;
+        boolean firstSub = false;
+        
+        for(int i= 0; i<data.length(); i++)
+        {
+            if(data.regionMatches(i, "0000", 0, 4))
+            {
+                if(numOnes%2 == 0)
+                    data = data.replaceFirst("0000", "b00v");
+                else
+                    data = data.replaceFirst("0000", "000v");
+                firstSub = true;
+                numOnes = 0;
+            }
+            switch (data.charAt(i)) {
+                case '1':
+                case 'b':
+                    if(lastPolarity > 0)
+                        signal += '-';
+                    else
+                        signal += '+';
+                    
+                    if(data.charAt(i)== '1' && firstSub)
+                        numOnes++;
+                    
+                    lastPolarity *= -1;
+                    break;
+                
+                case 'v':
+                    if(lastPolarity < 0)
+                        signal += '-';
+                    else
+                        signal += '+';
+                    break;
+                    
+                case '0':
+                    signal += '0';
+                    break;
+                default:
+                    break;
+                }
+        }
+        return signal;
+      }
+    
+     public static String mlt3(String data){
+        String signal = "";
+        int currentPolarity = 1;
+        boolean dirUp = true ;
+           
+        for(int i= 0; i<data.length(); i++)
+        {
+            switch (data.charAt(i)) {
+                case '1':
+                    if(currentPolarity == 1)
+                        signal += '+';
+                    else if(currentPolarity == 0)
+                        signal += '0';
+                    else
+                        signal += '-';
+                    
+                    if(currentPolarity == -1 || currentPolarity == 1)
+                        dirUp = !dirUp;
+                    
+                    if(dirUp)
+                        currentPolarity++;
+                    else
+                        currentPolarity--;
+                    
+                    break;
+                
+                case '0':
+                    if(i == 0)
+                        signal += '+';
+                    else
+                        signal += signal.charAt(i-1);
+                    break;
+                default:
+                    break;
+                }
+        }
+        return signal;
+      }
+     
+    }
+
+    
+    
+      
+    
+    
+
---
+++ src/linecodesgui/LineCodesGUI.java
@@ -0,0 +1,154 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package linecodesgui;
+
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author mhame
+ */
+public class LineCodesGUI extends Application {
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+     
+        DigitalWave w = new DigitalWave(DigitalWave.mlt3("0110010110100100001"));
+        //DigitalWave w = new DigitalWave(DigitalWave.hdb3("01000000001011000001"));
+        //DigitalWave w = new DigitalWave(DigitalWave.b8zs("0100100000000101"));
+        Pane pane = new Pane();
+       
+        pane.getChildren().addAll(w.lines);
+        
+        
+        Scene scene = new Scene(pane, 300, 250);
+           
+        primaryStage.setTitle("Line Codes");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
+
+
+  /* 
+        
+        
+        
+        Circle c1 = new Circle(60,60,80);
+        c1.setFill(null);
+        c1.setStroke(Color.RED);
+        
+        Circle c2 = new Circle(60,60,80);
+        c2.setFill(null);
+        c2.setStroke(Color.BLUE);
+        
+        Circle c3 = new Circle(60,60,80);
+        c3.setFill(null);
+        c3.setStroke(Color.PURPLE);
+        
+        Button grow = new Button("Grow");
+       grow.setOnAction((ActionEvent event) -> {
+            c1.setRadius(c1.getRadius() + 5 );
+       });
+      
+        
+        
+        class ShrinkHandler implements EventHandler<ActionEvent>
+        {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if(c1.getRadius() > 5)
+                 c1.setRadius(c1.getRadius() - 5);
+        }}
+        
+         Button shrink = new Button("Shrink");
+         ShrinkHandler shrinker = new ShrinkHandler();
+         shrink.setOnAction(shrinker);
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        //grid.setRotate(5);
+        grid.setPadding(new Insets(10));
+        grid.setVgap(10);
+        grid.setHgap(10);
+        //root.getChildren().add(btn);
+         grid.add(c1, 0, 0);
+         grid.add(grow, 0, 1);
+         grid.add(c2, 1, 0);
+         grid.add(shrink, 1, 1);
+         
+        // StackPane pane = new StackPane(grid);
+         grid.setOnMouseClicked(e -> {
+            
+            if(e.getButton() == MouseButton.PRIMARY)
+                c1.setRadius(c1.getRadius() + 5 );
+            else if(e.getButton() == MouseButton.SECONDARY && c1.getRadius() > 5 )
+                c1.setRadius(c1.getRadius() - 5);
+            else if(e.getButton() == MouseButton.MIDDLE)
+            {
+                if(c1.getFill() == null)
+                    c1.setFill(Color.YELLOW);
+                else
+                    c1.setFill(null);
+            }
+            
+        });
+         
+//         grid.setOnKeyPressed(e ->{
+//             if(e.getCode() == KeyCode.W)
+//                 grid.setLayoutY(grid.getLayoutY() - 10);
+//             else if(e.getCode() == KeyCode.S)
+//                 grid.setLayoutY(grid.getLayoutY() + 10);
+//         });
+        
+         
+          
+        StackPane sp = new StackPane(grid, c3);
+        
+        
+        sp.setOnKeyPressed(e -> {
+             if(e.getCode() == KeyCode.W)
+                c3.setCenterY(c3.getCenterY() - 10);
+             else if(e.getCode() == KeyCode.S)
+                 c3.setCenterY(c3.getCenterY() + 10);
+             else if(e.getCode() == KeyCode.D)
+                 c3.setCenterX(c3.getCenterX() + 10);
+             else if(e.getCode() == KeyCode.A)
+                 c3.setCenterX(c3.getCenterX() - 10);
+             
+         });
+         sp.requestFocus();
+        
+        Scene scene = new Scene(sp, 300, 250);
+        
+        primaryStage.setTitle("Shiiiiiiiiiiiii");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        
+        
+     */
